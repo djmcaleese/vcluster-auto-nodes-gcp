9d8cab4152db6eed42b56ee111c086fd59bc7b9b
Declan McAleese <21492+djmcaleese@users.noreply.github.com>
2026-06-01 16:03:10 +0100

    Only terminate on maintenance when it's supported

--- node/locals.tf
+++ node/locals.tf
@@ -11,4 +11,10 @@ locals {
   service_account_email = nonsensitive(var.vcluster.nodeEnvironment.outputs.infrastructure["service_account_email"])
 
   instance_type = nonsensitive(var.vcluster.nodeType.spec.properties["instance-type"])
+
+  # GPU families can't live-migrate, so they must TERMINATE on host maintenance.
+  # Standard families reject TERMINATE (unless Spot), so they must MIGRATE.
+  machine_family       = split("-", local.instance_type)[0]
+  gpu_machine_families = ["a2", "a3", "a4", "g2"]
+  on_host_maintenance  = contains(local.gpu_machine_families, local.machine_family) ? "TERMINATE" : "MIGRATE"
 }
--- node/main.tf
+++ node/main.tf
@@ -59,7 +59,7 @@ module "instance_template" {
   tags               = ["allow-iap-ssh", local.vcluster_name] # for IAP SSH access
 
   machine_type         = local.instance_type
-  on_host_maintenance  = "TERMINATE"
+  on_host_maintenance  = local.on_host_maintenance
 
   source_image         = data.google_compute_image.img.self_link
   source_image_family  = data.google_compute_image.img.family
